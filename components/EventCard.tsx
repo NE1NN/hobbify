2d94a5fa49c254b1c618e4e0114f770d9edad036
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-12 23:24:46 +1100

    Add event card

---
+++ components/EventCard.tsx
@@ -0,0 +1,45 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+import { FontAwesome } from '@expo/vector-icons';
+
+export default function EventCard() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.image}></View>
+      <View style={styles.detailsContainer}>
+        <Text style={styles.eventName}>Soccer</Text>
+        <View style={styles.locationContainer}>
+          <FontAwesome name="map-marker" />
+          <Text>UNSW Village Green</Text>
+        </View>
+        <View>
+          <Text>Thursday, 11 November</Text>
+          <Text>at 4pm</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  detailsContainer: {
+    justifyContent: 'center',
+    gap: 20,
+  },
+  eventName: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  image: {
+    width: 150,
+    height: 200,
+    backgroundColor: 'black',
+    borderRadius: 20,
+  },
+  locationContainer: {
+    flexDirection: 'row',
+  },
+});
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import EventCard from '../components/EventCard';
 
 export default function HomeScreen() {
   return (
-    <Text>Home</Text>
-  )
+    <SafeAreaView>
+      <Text>Welcome Back, User</Text>
+      <Text>Recommendations</Text>
+      <EventCard />
+    </SafeAreaView>
+  );
 }
+
