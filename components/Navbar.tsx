a02f08a93f5e1bb38b4ad04c2fd236b154b8aac6
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-12 22:07:10 +1100

    Add icons to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,47 @@
 import React from 'react';
-import { View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { FontAwesome } from '@expo/vector-icons';
+import SearchScreen from '../screens/SearchScreen';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navbar() {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          tabBarIcon: ({ size }) => (
-            <Ionicons name="md-document-text" size={size} />
-          ),
+          tabBarIcon: ({ size }) => <FontAwesome name="home" size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ size }) => <FontAwesome name="search" size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Event"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ size }) => <FontAwesome name="calendar" size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ size }) => <FontAwesome name="user" size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SearchScreen}
+        options={{
+          tabBarIcon: ({ size }) => <FontAwesome name="cog" size={size} />,
         }}
       />
     </Tab.Navigator>
---
+++ screens/SearchScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function SearchScreen() {
+  return (
+    <Text>SearchScreen</Text>
+  )
+}
