de2c9dc6653859e5f4504e70a16a3a011095efa4
Edrick Koda <z5349845@unsw.edu.au>
2023-11-20 14:51:47 +1100

    Implemented the TopTabBar

---
+++ screens/Events/History.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import EventCard from "../../components/EventCard";
+import { Event, getEvents } from "../../utils/api";
+import UserEventCard from "../../components/UserEventCard";
+
+export default function History() {
+  const [events, setEvents] = useState<Event[]>([]);
+
+  useEffect(() => {
+    const populateEvents = async () => {
+      const events = await getEvents();
+      setEvents(events);
+    };
+    populateEvents();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.SafeAreaView}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <TouchableOpacity style={styles.createEventButton}>
+          <Text style={styles.createEventText}>Create an Event</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.sectionHeading}>Review Users</Text>
+        <View>
+          <UserEventCard />
+          <UserEventCard />
+          <UserEventCard />
+        </View>
+
+        <Text style={styles.sectionHeading}>Past Events</Text>
+        <View style={styles.eventsContainer}>
+          {events.map((event, idx) => (
+            <EventCard
+              key={idx}
+              name={event.name}
+              location={event.location}
+              thumbnail={event.thumbnail}
+              time={event.time}
+              eventId={event.eventId}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  SafeAreaView: {
+    marginHorizontal: 25,
+  },
+  sectionHeading: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  eventsContainer: {
+    gap: 10,
+  },
+  createEventButton: {
+    marginVertical: 24,
+    backgroundColor: "#28B67E",
+    borderRadius: 15,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  createEventText: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "white",
+    textAlign: "center",
+  },
+});
--- screens/EventsScreen.tsx
+++ screens/EventsScreen.tsx
@@ -1,10 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Button, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import EventCard from '../components/EventCard';
-import { getEvents } from '../utils/api';
-import { Event } from '../utils/api';
-import UserEventCard from '../components/UserEventCard';
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import EventCard from "../components/EventCard";
+import { getEvents } from "../utils/api";
+import { Event } from "../utils/api";
+import UserEventCard from "../components/UserEventCard";
+import { EventsTopBar } from "../components/EventsTopBar";
 
 export default function EventsScreen() {
   const [events, setEvents] = useState<Event[]>([]);
@@ -19,35 +27,7 @@ export default function EventsScreen() {
 
   return (
     <SafeAreaView style={styles.SafeAreaView}>
-      <ScrollView showsVerticalScrollIndicator={false}>
-
-        <TouchableOpacity style={styles.createEventButton}>
-          <Text style={styles.createEventText}>Create an Event</Text>
-        </TouchableOpacity>
-
-
-        <Text style={styles.sectionHeading}>Review Users</Text>
-        <View>
-          <UserEventCard/>
-          <UserEventCard/>
-          <UserEventCard/>
-        </View>
-
-
-        <Text style={styles.sectionHeading}>Past Events</Text>
-        <View style={styles.eventsContainer}>
-          {events.map((event, idx) => (
-            <EventCard
-              key={idx}
-              name={event.name}
-              location={event.location}
-              thumbnail={event.thumbnail}
-              time={event.time}
-              eventId={event.eventId}
-            />
-          ))}
-        </View>
-      </ScrollView>
+      <EventsTopBar />
     </SafeAreaView>
   );
 }
@@ -56,10 +36,11 @@ const styles = StyleSheet.create({
   SafeAreaView: {
     marginHorizontal: 25,
     marginTop: 30,
+    backgroundColor: "white",
   },
   sectionHeading: {
     fontSize: 28,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   eventsContainer: {
@@ -69,13 +50,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#28B67E",
     borderRadius: 15,
     height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   createEventText: {
     fontSize: 20,
-    fontWeight: '700',
-    color: 'white',
-    textAlign: 'center',
-  }
+    fontWeight: "700",
+    color: "white",
+    textAlign: "center",
+  },
 });
