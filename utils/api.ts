1ba26c9bf07aa95fff11db3138309e870fdf1df7
Edrick Koda <z5349845@unsw.edu.au>
2023-11-21 16:52:48 +1100

    fixed bugs

--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,18 @@
-import * as FileSystem from 'expo-file-system';
-const filePath = FileSystem.documentDirectory + 'data.json';
-import { collection, getDocs, query, where, doc, getDoc, setDoc, addDoc } from 'firebase/firestore';
-import { db } from '../firebaseConfig';
-import { Timestamp } from 'firebase/firestore';
-import { generateSimpleToken, generateUserId } from './helpers';
+import * as FileSystem from "expo-file-system";
+const filePath = FileSystem.documentDirectory + "data.json";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  doc,
+  getDoc,
+  setDoc,
+  addDoc,
+} from "firebase/firestore";
+import { db } from "../firebaseConfig";
+import { Timestamp } from "firebase/firestore";
+import { generateSimpleToken, generateUserId } from "./helpers";
 
 type User = {
   userId: number;
@@ -15,10 +24,11 @@ export type Event = {
   creatorId: number;
   thumbnail: string;
   name: string;
-  desc:string;
+  nameLowered: string;
+  description: string;
   location: string;
   time: Timestamp;
-  isPublic: boolean
+  isPublic: boolean;
   members: number[];
   likes: number[];
 };
@@ -32,10 +42,10 @@ export type createEventDetails = {
   date: Date;
   time: Date;
   isPublic: boolean;
-}
+};
 
 export const getEvents = async () => {
-  const eventsCol = collection(db, 'events');
+  const eventsCol = collection(db, "events");
   const eventsDocs = await getDocs(eventsCol);
 
   const events: Event[] = eventsDocs.docs.map(
@@ -49,32 +59,36 @@ export const getEvents = async () => {
 };
 
 export const searchEvent = async (searchValue: string) => {
-  const eventsCol = collection(db, 'events');
+  const eventsCol = collection(db, "events");
   const eventsQuery = query(
     eventsCol,
-    where('nameLowered', '>=', searchValue.toLowerCase()),
-    where('nameLowered', '<=', searchValue.toLowerCase() + '\uf8ff')
+    where("nameLowered", ">=", searchValue.toLowerCase()),
+    where("nameLowered", "<=", searchValue.toLowerCase() + "\uf8ff")
   );
   const eventDocs = await getDocs(eventsQuery);
   return eventDocs;
 };
 
 export const getEvent = async (eventId: string) => {
-  const eventsCol = collection(db, 'events')
-  const eventRef = doc(eventsCol, eventId)
-  const eventDoc = await getDoc(eventRef)
-  return eventDoc
-}
+  const eventsCol = collection(db, "events");
+  const eventRef = doc(eventsCol, eventId);
+  const eventDoc = await getDoc(eventRef);
+  return eventDoc;
+};
 
-export const registerUser = async (username: string, email: string, password: string) => {
-  const userCol = collection(db, 'users')
+export const registerUser = async (
+  username: string,
+  email: string,
+  password: string
+) => {
+  const userCol = collection(db, "users");
 
   const usernameQuery = query(userCol, where("username", "==", username));
   const querySnapshot = await getDocs(usernameQuery);
 
   if (!querySnapshot.empty) {
     // Username already exists
-    throw new Error('Username already taken');
+    throw new Error("Username already taken");
   }
 
   let user = {
@@ -83,69 +97,64 @@ export const registerUser = async (username: string, email: string, password: st
     joinedEvents: [],
     username,
     password,
-    userId: generateUserId()
-  }
+    userId: generateUserId(),
+  };
 
-  await addDoc(userCol, user)
+  await addDoc(userCol, user);
   return {
     token: generateSimpleToken(),
-    userId: user.userId
-  }
-}
+    userId: user.userId,
+  };
+};
 
 export const loginUser = async (username: string, password: string) => {
-  const usersCol = collection(db, 'users')
+  const usersCol = collection(db, "users");
   const q = query(
-    usersCol, 
+    usersCol,
     where("username", "==", username),
-    where("password", "==", password )
+    where("password", "==", password)
   );
   const querySnapshot = await getDocs(q);
   try {
     if (!querySnapshot.empty) {
       const userDoc = querySnapshot.docs[0];
       const userData = userDoc.data(); // Retrieve the data of the user document
-  
+
       const userId = userData.userId; // Access the 'userId' field
-  
+
       return {
         token: generateSimpleToken(),
-        userId: userId // Use the 'userId' from the document data
-      }; 
-  
+        userId: userId, // Use the 'userId' from the document data
+      };
     } else {
       return null;
     }
   } catch (e) {
-    console.log('meki', e)
+    console.log("meki", e);
   }
-}
+};
 
 export const getUserDetail = async (userId: number) => {
-  const usersCol = collection(db, 'users')
-  const q = query(
-    usersCol, 
-    where("userId", "==", userId),
-  );
+  const usersCol = collection(db, "users");
+  const q = query(usersCol, where("userId", "==", userId));
   const querySnapshot = await getDocs(q);
 
   if (!querySnapshot.empty) {
     const userDoc = querySnapshot.docs[0];
     const userData = userDoc.data(); // Retrieve the data of the user document
 
-    const username = userData.username; 
+    const username = userData.username;
 
-    return username
-
+    return username;
   } else {
     return null;
   }
-}
+};
 
 export const createEvent = async (props: createEventDetails) => {
-  const { creatorId, thumbnail, name, desc, location, date, time, isPublic } = props
+  const { creatorId, thumbnail, name, desc, location, date, time, isPublic } =
+    props;
   const eventsCol = collection(db, "events");
-
 
   // Extracting date parts
   let year = date.getFullYear();
@@ -159,9 +168,17 @@ export const createEvent = async (props: createEventDetails) => {
   let milliseconds = time.getMilliseconds();
 
   // Creating a new Date object with combined date and time
-  let combinedDateTime = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+  let combinedDateTime = new Date(
+    year,
+    month,
+    day,
+    hours,
+    minutes,
+    seconds,
+    milliseconds
+  );
 
-  const timestamp = Timestamp.fromDate(combinedDateTime)
+  const timestamp = Timestamp.fromDate(combinedDateTime);
 
   let event = {
     creatorId,
@@ -174,9 +191,9 @@ export const createEvent = async (props: createEventDetails) => {
     time: timestamp,
     isPublic,
     membersLimit: 0,
-    likes: []
+    likes: [],
   };
 
   await addDoc(eventsCol, event);
-  return {}
-}
+  return {};
+};
